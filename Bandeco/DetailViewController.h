816b9981f37a4feb396ffb8637a99715a7e3aa70
Pedro Goes <pedro.pecanha.goes@gmail.com>
2012-03-30 18:44:40 -0300

    Initial Commit

---
+++ Bandeco/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Bandeco
+//
+//  Created by Pedro Góes on 30/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController <UISplitViewControllerDelegate>
+
+@property (strong, nonatomic) id detailItem;
+
+@property (strong, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
---
+++ Bandeco/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  Bandeco
+//
+//  Created by Pedro Góes on 30/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@end
